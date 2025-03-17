dc6e10b05b5b98e49d1435f41a85003bf26ea75f
Anand <moreanand111011@gmail.com>
2025-03-17 13:09:55 +0530

    i2ispecialist project files added

---
+++ controller/UserController.js
@@ -0,0 +1,24 @@
+import User from "../models/userModel.js";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if user already exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Create new user
+    const user = await User.create({ name, email, password });
+    
+    if (user) {
+      res.status(201).json({ _id: user._id, email: user.email });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
